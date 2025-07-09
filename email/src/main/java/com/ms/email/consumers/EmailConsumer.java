e3562a9a9b82cefec211343ec829503e2234161a
Guilherme Machado <guimiguel.machado@gmail.com>
2025-07-09 16:00:14 -0300

    Finished implementation and tested email sender system

--- email/src/main/java/com/ms/email/consumers/EmailConsumer.java
+++ email/src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -1,15 +1,26 @@
 package com.ms.email.consumers;
 
 import com.ms.email.dtos.EmailRecordDTO;
+import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
 
+    final EmailService service;
+
+    public EmailConsumer(EmailService service) {
+        this.service = service;
+    }
+
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDTO emailRecordDTO){
-        System.out.println(emailRecordDTO.emailTo());
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailRecordDTO, emailModel);
+        service.sendEmail(emailModel);
     }
 }
