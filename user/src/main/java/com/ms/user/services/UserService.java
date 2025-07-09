ced0c80850ec63921f9ae05b859d0151e8b5f25d
Guilherme Machado <guimiguel.machado@gmail.com>
2025-07-09 15:16:55 -0300

    Created Producer

--- user/src/main/java/com/ms/user/services/UserService.java
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ms.user.services;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
 import com.ms.user.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,14 +10,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     final UserRepository repo;
+    final UserProducer prod;
 
-    public UserService(UserRepository repo) {
+    public UserService(UserRepository repo, UserProducer prod) {
         this.repo = repo;
+        this.prod = prod;
     }
 
     @Transactional
     public UserModel save(UserModel userModel){
-        return repo.save(userModel);
+        userModel = repo.save(userModel);
+        prod.publishEmailMessage(userModel);
+        return userModel;
     }
 
 }
